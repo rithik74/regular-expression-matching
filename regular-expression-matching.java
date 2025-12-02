175a3089d65adaff86cbb6bc9fa47f5cbdd1248e
Rithik Mohaanprakaas <rithikmohaan@gmail.com>
2025-12-02 09:24:56 +0530

    Implement regex matching with support for '.' and '*'

---
+++ regular-expression-matching.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+
+        boolean[][] dp = new boolean[m + 1][n + 1];
+
+        dp[0][0] = true;  // empty string matches empty pattern
+
+        // Handle patterns like a*, a*b*, a*b*c*
+        for (int j = 2; j <= n; j++) {
+            if (p.charAt(j - 1) == '*' && dp[0][j - 2]) {
+                dp[0][j] = true;
+            }
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+
+                // Case 1: direct match or '.'
+                if (p.charAt(j - 1) == s.charAt(i - 1) || p.charAt(j - 1) == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+
+                // Case 2: '*'
+                else if (p.charAt(j - 1) == '*') {
+                    // Option 1: ignore the character before '*' (zero occurrences)
+                    dp[i][j] = dp[i][j - 2];
+
+                    // Option 2: use '*' multiple times
+                    if (p.charAt(j - 2) == s.charAt(i - 1) || p.charAt(j - 2) == '.') {
+                        dp[i][j] |= dp[i - 1][j];
+                    }
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
